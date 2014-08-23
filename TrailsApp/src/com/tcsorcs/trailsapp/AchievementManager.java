52b081c86ff317832c011cd52d57a506f9e5dd2e
HypnoticSheep <mike.keck@yahoo.com>
2014-08-22 20:32:51 -0400

    Added hook for DistMan
    Added in a hook for DistanceManager to call to manually add an
    achievement.

--- TrailsApp/src/com/tcsorcs/trailsapp/AchievementManager.java
+++ TrailsApp/src/com/tcsorcs/trailsapp/AchievementManager.java
@@ -1,8 +1,41 @@
 package com.tcsorcs.trailsapp;
 
+import android.R.string;
+
 class AchievementManager {
-  public static AchievementManager getInstance() {
-	  return AchievementManager.instance;
-  }
-  static AchievementManager instance = new AchievementManager();
+	
+	private static string[] grantedAchievements;
+	private static int arrayLength = 20;
+	private static int currLoc;
+	
+	public AchievementManager(){
+		grantedAchievements = new string[arrayLength];
+		currLoc = 0;
+	}
+	
+	public static AchievementManager getInstance() {
+		return AchievementManager.instance;
+	}
+	static AchievementManager instance = new AchievementManager();
+  
+	/// AwardAchievement
+	/// Awards an achievement directly (testing only)
+	/// achieveName: Name of the achievement awarded
+	/// achieveDesc: Description of awarded achievement
+	/// Returns a bool of whether the achievement was successfully awarded.
+	///  |-> False if achievement has already been granted.
+	public static boolean AwardAchievement(string achieveName, string achieveDesc){
+		// TODO: Display achievement through DisplayManager
+		// Check if we've pushed this cheevo in yet
+		for(int i = 0; i < currLoc; ++i){
+			if(grantedAchievements[i] == achieveName){
+				// If we have, return false and we're done
+				return false;
+			}
+		}
+		// Otherwise push it in and return true.
+		grantedAchievements[currLoc] = achieveName;
+		++currLoc;
+		return true;
+	}
 }
