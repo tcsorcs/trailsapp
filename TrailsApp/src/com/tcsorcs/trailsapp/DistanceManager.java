6a5250d604c12970a1e9e394adc1766f50955173
SarahH <ladyofpoisons@gmail.com>
2014-12-09 18:22:59 -0500

    Messing with GitHub and readded warning to master branch

--- TrailsApp/src/com/tcsorcs/trailsapp/DistanceManager.java
+++ TrailsApp/src/com/tcsorcs/trailsapp/DistanceManager.java
@@ -18,7 +18,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/* v0.0 - Demo only version
+/* MASTER BRANCH IS NOT BEING ACTIVELY UPDATED!
  *  Only Excellence trail codes available
  *  No database connection 
  *  Only works if codes are scanned in this order, either forward or backwards ExceEnt <-> L21 <-> L20 <-> L18 <-> DepeEnt
