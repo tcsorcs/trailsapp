354a25bf80015624e7f8df23b2448a8a93dde13b
SarahH <ladyofpoisons@gmail.com>
2015-03-03 18:23:01 -0500

    Updataed pathfinder

--- TrailsStudio/app/src/main/java/com/tcsorcs/trailsapp/managers/DistanceManager.java
+++ TrailsStudio/app/src/main/java/com/tcsorcs/trailsapp/managers/DistanceManager.java
@@ -6,54 +6,54 @@ import java.util.Comparator;
 import java.util.Stack;
 import java.util.TreeSet;
 
-/* v1.0 - Full version
- *  
- *  Currently NOT FUNCTIONAL - needs actual hooks to connect to database and hooks for display manager to be implemented
- *  DNE = does not exist
- *  Updated 2/17/2015
- */
- /* Below are ideas, some outdated. Will clean up later.
- *   Stuff Distance manager will need for 1 session
-  *  >Each distance code scanned needs this information stored: 
-  *    Name (String), order scanned (int), time scanned (long)
-  *  >Need distances between adjacent codes stored as:
-  *    Code Name1 (String), Code Name2 (String), distance (double)
-  *   It would be super dooper great if the order each code is listed is consistent - preferably 
-  *     alphabetically or some other easy way to sort.
-  *  
-  *  Stuff for stats recording (expanded from above)
-  *  Pace Stats Recording:
-  *    Code Name1 (String), Code Name2 (String), distance (double), currentPace (double), averagePace (double)
-  *  Is there a way to store an array/mutable sized storage container in the DB? If so, then just use that
-  *    to record all paces per section of the trail, and calculate average from that data when we need to.
-  *    If so, the stats recording/entry for each path section could be:
-  *    Code Name1 (String), Code Name2 (String), distance (double), currentPace (double), pastPaces (array of doubles)
-  *
-  *  Routes Stats Recording:
-  *  Basic idea is we have a trail identifier (more later), a tally counting the times walked, and a 
-  *    container to store all paces (as described in Pace Stats Recording)
-  *    ID, timesWalked (int), pastPaces? (array of doubles)
-  *  If this identifier is a nickname (could be nickname or id) of all the logical trail loops we could 
-  *    make an entry for each in the DB before the app is ever run. 
-  *    - would need a conversion table from segments to trail name
-  *    = would still need the ability to add new "loops" on the fly (if you only scanned part of a loop)
-  *    + more human readable for debugging
-  *    +/- we know we have (the majority of) the space we'd need, but there would be wasted space
-  *    + don't care about the start/end
-  *    ? if a walk consists of multiple loops, do we tally each sub loop, or make a new entry for the
-  *       total loop
-  *    ? may not need pastPaces section if we make something grab paces for each segment of each trail
-  *  If this identifier is a container (list/array etc like for pastPaces - assuming this is possible) we
-  *    could dynamically add new trail loops as the user uses them
-  *    - each trail loop needs to have a standard start/end, or there will be duplicates
-  *    +/- DB increases as needed, but total space of app will vary more
-  *    - will need a smarter search algorithm to decide which trail we've just walked
-  *    + do not need a pastPaces column (can calculate it as needed based on pace for each segment)
-  *    -/? related to ? from above, if segments are not broken down, may have many long trails only walked once
-  *
-  *  Or could only store info that we need for an achievement (ex, only tally the Kurtis F Trail walks)
-  *    would simplify the problems above
-  */
+    /* v1.0 - Full version
+     *
+     *  Currently NOT FUNCTIONAL - needs actual hooks to connect to database and hooks for display manager to be implemented
+     *  DNE = does not exist
+     *  Updated 2/17/2015
+     */
+     /* Below are ideas, some outdated. Will clean up later.
+     *   Stuff Distance manager will need for 1 session
+      *  >Each distance code scanned needs this information stored:
+      *    Name (String), order scanned (int), time scanned (long)
+      *  >Need distances between adjacent codes stored as:
+      *    Code Name1 (String), Code Name2 (String), distance (double)
+      *   It would be super dooper great if the order each code is listed is consistent - preferably
+      *     alphabetically or some other easy way to sort.
+      *
+      *  Stuff for stats recording (expanded from above)
+      *  Pace Stats Recording:
+      *    Code Name1 (String), Code Name2 (String), distance (double), currentPace (double), averagePace (double)
+      *  Is there a way to store an array/mutable sized storage container in the DB? If so, then just use that
+      *    to record all paces per section of the trail, and calculate average from that data when we need to.
+      *    If so, the stats recording/entry for each path section could be:
+      *    Code Name1 (String), Code Name2 (String), distance (double), currentPace (double), pastPaces (array of doubles)
+      *
+      *  Routes Stats Recording:
+      *  Basic idea is we have a trail identifier (more later), a tally counting the times walked, and a
+      *    container to store all paces (as described in Pace Stats Recording)
+      *    ID, timesWalked (int), pastPaces? (array of doubles)
+      *  If this identifier is a nickname (could be nickname or id) of all the logical trail loops we could
+      *    make an entry for each in the DB before the app is ever run.
+      *    - would need a conversion table from segments to trail name
+      *    = would still need the ability to add new "loops" on the fly (if you only scanned part of a loop)
+      *    + more human readable for debugging
+      *    +/- we know we have (the majority of) the space we'd need, but there would be wasted space
+      *    + don't care about the start/end
+      *    ? if a walk consists of multiple loops, do we tally each sub loop, or make a new entry for the
+      *       total loop
+      *    ? may not need pastPaces section if we make something grab paces for each segment of each trail
+      *  If this identifier is a container (list/array etc like for pastPaces - assuming this is possible) we
+      *    could dynamically add new trail loops as the user uses them
+      *    - each trail loop needs to have a standard start/end, or there will be duplicates
+      *    +/- DB increases as needed, but total space of app will vary more
+      *    - will need a smarter search algorithm to decide which trail we've just walked
+      *    + do not need a pastPaces column (can calculate it as needed based on pace for each segment)
+      *    -/? related to ? from above, if segments are not broken down, may have many long trails only walked once
+      *
+      *  Or could only store info that we need for an achievement (ex, only tally the Kurtis F Trail walks)
+      *    would simplify the problems above
+      */
 
 /*
  * Assumptions made about classes that don't exist:
@@ -131,7 +131,7 @@ public class DistanceManager {
         else {//need to pathfind
             smarterPathFinder(codeName);
         }
-    //debug System.out.println("DistanceManager.processQRCodes end");
+        //debug System.out.println("DistanceManager.processQRCodes end");
     }
 
     /* DEPRICATED
@@ -160,10 +160,10 @@ public class DistanceManager {
         return pathList;
     }
 */
-    /*
-     * Returns total distance walked note distance is calculated each time a
-     * distanceQR code is scanned
-     */
+        /*
+         * Returns total distance walked note distance is calculated each time a
+         * distanceQR code is scanned
+         */
     public double getDistance() {
         return this.totalDistance;
     }
@@ -195,81 +195,87 @@ public class DistanceManager {
     /*****
      * stupidPathFinder and stupidDistanceCalc will be replaces with smarterPathFinder soon.
 
-    // Checks off markers assuming we skipped some
-    // default is start at ExeEnt
-    private void stupidPathFinder(int currentScan) {
-        int endPoint = 0;
-        int i;
+     // Checks off markers assuming we skipped some
+     // default is start at ExeEnt
+     private void stupidPathFinder(int currentScan) {
+     int endPoint = 0;
+     int i;
 
-        // Stupid find entrance
-        if (firstCodeScanned.equals("DepeEnt")
-                || firstCodeScanned.equals("L18")) {
-            endPoint = 4;
-        }
+     // Stupid find entrance
+     if (firstCodeScanned.equals("DepeEnt")
+     || firstCodeScanned.equals("L18")) {
+     endPoint = 4;
+     }
 
-        // fill in spaces between + marks off current
-        if (endPoint < currentScan) {
-            for (i = endPoint; i <= currentScan; i++) {
-                this.markers[i] = true;
-            }
-        } else {
-            for (i = currentScan; i <= endPoint; i++) {
-                this.markers[i] = true;
-            }
-        }
-        stupidDistanceCalc();
-    }
+     // fill in spaces between + marks off current
+     if (endPoint < currentScan) {
+     for (i = endPoint; i <= currentScan; i++) {
+     this.markers[i] = true;
+     }
+     } else {
+     for (i = currentScan; i <= endPoint; i++) {
+     this.markers[i] = true;
+     }
+     }
+     stupidDistanceCalc();
+     }
 
-    // calculates distance
-    private void stupidDistanceCalc() {
-        double calcDistance = 0.0;
+     // calculates distance
+     private void stupidDistanceCalc() {
+     double calcDistance = 0.0;
 
-        if (this.markers[0] && this.markers[1]) {
-            calcDistance += ExceEnt_L21;
-        }
-        if (this.markers[1] && this.markers[2]) {
-            calcDistance += L20_L21;
-        }
-        if (this.markers[2] && this.markers[3]) {
-            calcDistance += L18_L20;
-        }
-        if (this.markers[3] && this.markers[4]) {
-            calcDistance += DepeEnt_L18;
-        }
+     if (this.markers[0] && this.markers[1]) {
+     calcDistance += ExceEnt_L21;
+     }
+     if (this.markers[1] && this.markers[2]) {
+     calcDistance += L20_L21;
+     }
+     if (this.markers[2] && this.markers[3]) {
+     calcDistance += L18_L20;
+     }
+     if (this.markers[3] && this.markers[4]) {
+     calcDistance += DepeEnt_L18;
+     }
 
-        this.totalDistance = calcDistance;
-    }
-*/
-    /*A smarter path finder that handles the whole trail
-     * handles skipped points, crossing the road ...
-     *
-     * assumes is not passed the first point seen
+     this.totalDistance = calcDistance;
+     }
      */
+        /*A smarter path finder that handles the whole trail
+         * handles skipped points, crossing the road ...
+         *
+         * assumes is not passed the first point seen
+         */
 
     private void smarterPathFinder(String currentScan){
         String lastScan; //most recent point from global path stack
-        ArrayList<Segment> currentSegments; //list of segments, returned from DB //DNE
-        Segment aSegment; //current segment we're working with //DNE
+        ArrayList<Segment> attachedSegments; //list of segments, returned from DB //DNE
+        Segment currentSegment = null; //current segment we're working with //DNE
         boolean pointsAdjacent = false; //if two points are on the same segment
 
         //get most recent point from path
         lastScan = path.peek();
 
         //get all segments with currentScan
-        currentSegments = getSegmentsWithPoint(currentScan); //DNE
+        attachedSegments = getSegmentsWithPoint(currentScan); //DNE
+
+        //if getSegmentsWithPoint grabs nothing b/c error, don't continue because will break
+        if (attachedSegments.size() < 1){
+            System.err.println("Scanned point "+currentScan+" does not seem to be part of a segment. Pathfinding cannot be done. Returning to processQRCode");
+            return;
+        }
 
         //checks if last 2 points scanned are on the same segment
-        for (int i = 0; i < currentSegments.size() && !pointsAdjacent; i++){
-            aSegment = currentSegments.get(i);
-            pointsAdjacent = aSegment.segmentHasPoints(lastScan, currentScan); //DNE
+        for (int i = 0; i < attachedSegments.size() && !pointsAdjacent; i++){
+            currentSegment = attachedSegments.get(i);
+            pointsAdjacent = currentSegment.segmentHasPoints(lastScan, currentScan); //DNE
         }
 
         //we know our next segment, yay!
         if(pointsAdjacent){
             path.push(currentScan);
-            totalDistance += aSegment.segmentDistance;//DNE
+            totalDistance += currentSegment.segmentDistance; //DNE //if not initialized, will not be hit
             //display segment
-            DisplayManager.drawSegment(aSegment);//DNE
+            DisplayManager.drawSegment(currentSegment);//DNE
 
             //display new point
             DisplayManager.drawMarker(getLocation(currentScan), false, true); //DNE
@@ -277,17 +283,16 @@ public class DistanceManager {
         }
 
         //if points are not adjacent, we need to pathfind!
-        //note, this starts with the currently scanned point and moves "backwards" toward the existing path
 
-        //eliminate segments crossing road if points not on different sides
+        //(add for optimization?) eliminate segments crossing road if points not on different sides
 
-        /*
-        ideas: order possible segments by length, shortest add next possible length (if multiple options,
-        clone and add all options to list - without duplicating segments on same path), resort, find next
-        shortest, add next possible lenght, resort etc until find end of existing path
-        Tree might work? Needs to be able to self-order length
+            /*
+            ideas: order possible segments by length, shortest add next possible length (if multiple options,
+            clone and add all options to list - without duplicating segments on same path), resort, find next
+            shortest, add next possible lenght, resort etc until find end of existing path
+            Tree might work? Needs to be able to self-order length
 
-         */
+             */
 
 
         //get previous scanned point
@@ -297,11 +302,17 @@ public class DistanceManager {
 
         //And magic happens - 100% chance this will fail first time it's run...
         //have currentScan(start with this), lastScan(end of the path we've already built)
+            /*
+             * pathfinder will build unknown path starting with most recent scanned, last point added to
+             * pathfinder list will be last point on main path. We then remove last point from list
+             * and pop it onto path (adding distance as we go) so that last point left in pathfinder list
+             * is most recent point scanned, and now the end of the main path.
+             */
 
         //MUST BE FIXED!!!!!
 
         //local class for "node"
-        class Node implements Comparator<Node> {
+        class Node implements Comparable<Node> {
             String scanName; //QR code name
             Node parent; //parent node, or null
             double nodeDistance; //total distance from start of subpath
@@ -312,31 +323,27 @@ public class DistanceManager {
                 nodeDistance = aDistance;
             }
 
-            public int compare(Node right, Node left) {
-                if (right.nodeDistance < left.nodeDistance) {
-                    return -1;
-                } else if (right.nodeDistance == left.nodeDistance){
-                    return 0;
-                } else {
-                    return 1;
-                }
+            //Potential error - right and left might need to be switched...
+            @Override
+            public int compareTo(Node left) {
+                return Double.compare(this.nodeDistance, left.nodeDistance);
             }
         }//local Node class
 
         Node currentNode = new Node(currentScan, null, 0.0);//the first node we add - put here for easier visibility
-        TreeSet<Node> subPath = new TreeSet(Node); //NOT CORRECT - want to implement the node comparator
+        TreeSet<Node> subPath = new TreeSet<Node>(); //hopefully knows to use Node's compareTo...
 
-        subPath.add(currentNode);
-        Node shortest = subPath.first();
+        subPath.add(currentNode);//add first
+        Node shortest = subPath.first(); //get shortest (here only)
 
         ArrayList<Segment> nextSegments = getSegmentsWithPoint(shortest.scanName);
-        Segment aSegment;
-        Node aNode;
+        Segment subSegment;
+        Node subNode;
 
         while (!nextSegments.isEmpty()){
-            aSegment = nextSegments.popNext(); //THIS ISN'T REAL
-            aNode = new Node(aSegment.name(), shortest, (aSegment.distance+shortest.nodeDistance));
-            subPath.add(aNode);
+            subSegment = nextSegments.remove(0); //removes and returns, decreasing list //(is this less optimal than using a for loop?)
+            subNode = new Node(subSegment.name(), shortest, (subSegment.distance+shortest.nodeDistance));
+            subPath.add(subNode);
         }
 
 
