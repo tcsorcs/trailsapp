3e06ee80d543653468a088ef2ab9301d313f6434
Mike Keck <mike.keck@yahoo.com>
2015-12-07 18:27:05 -0500

    stopped breaking everything
    sorry

--- TrailsStudio/app/src/main/java/com/tcsorcs/trailsapp/helpers/Goal.java
+++ TrailsStudio/app/src/main/java/com/tcsorcs/trailsapp/helpers/Goal.java
@@ -1,5 +1,8 @@
 package com.tcsorcs.trailsapp.helpers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Mike on 8/10/2015.
  */
@@ -31,22 +34,27 @@ public class Goal {
         return childTasks;
     }
 
+    // Constructors
+    public Goal(){
+        this.name = "New Goal";
+        this.singleSession = false;
+        this.timeLimit = 0.0f;
+        this.currTaskID = 0;
+    }
     public Goal(String name, boolean singleSession, float timeLimit) {
         this.name = name;
         this.singleSession = singleSession;
         this.timeLimit = timeLimit;
         this.currTaskID = 0;
     }
-
-
     /* Copy constructor
     * This constructor and the subsequent method are used to make
     * this Goal object a copy of another Goal object.
     */
     public Goal(Goal toCopy) {
         this.name = "Copy of " + toCopy.getName();
-        this.singleSession = toCopy.getSingleSession();
-        this.timeLimit = toCopy.timeLimit();
+        this.singleSession = toCopy.isSingleSession();
+        this.timeLimit = toCopy.getTimeLimit();
         copyChildTasks(toCopy.getChildTasks());
     }
 
@@ -68,7 +76,7 @@ public class Goal {
         Iterator<Task> i = childTasks.iterator();
         while(i.hasNext()){
             Task childTask = i.next();
-            if(!childTask.isComplete) {
+            if(!childTask.isComplete()) {
                 boolean taskComplete = childTask.addAmount(amt);
                 if (taskComplete) {
                     //TODO: display completion message
@@ -81,15 +89,15 @@ public class Goal {
         Iterator<Task> i = childTasks.iterator();
         while(i.hasNext()) {
             Task childTask = i.next();
-            if(childTask.GetTaskID() == taskID){
-                childTasks.delete(childTask);
+            if(childTask.getTaskID() == taskID){
+                childTasks.remove(childTask);
                 break;
             }
         }
     }
     /// Faster, but requires the full Task object
     public void DeleteTask(Task toDelete){
-        childTasks.delete(toDelete);
+        childTasks.remove(toDelete); 
     }
 
 }
